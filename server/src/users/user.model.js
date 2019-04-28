01905e66da41a271d3196e8510820b391ce38a01
Osher Levy <osher.levy@me.com>
2019-04-28 20:11:03 +0300

    edited user page

--- server/src/users/user.model.js
+++ server/src/users/user.model.js
@@ -6,6 +6,7 @@ const userSchema = mongoose.Schema({
     birthDate: { type: Date, required: false },
     avatarPic: { type: String, reqired: false },
     coverPic: { type: String, reqired: false },
+    alernateName: { type: String, reqired: false },
     registrationDate: { type: Date, default: Date.Now }
 });
 
--- src/users/User.js
+++ src/users/User.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import Cover from './cover';
 
 class User extends Component {
+    state = { isLoaded: false, user: {} };
+
+    componentDidMount() {
+        fetch('http://localhost:8080/users/5cc5b97ed4b68d29ece7e7fd')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    user: data,
+                    isLoaded: true
+                });
+            });
+    }
+
     render() {
+        const { isLoaded, user } = this.state;
+        if (!isLoaded) {
+            return <div>Loading...</div>;
+        }
         return (
             <div>
-                <Cover url="https://scontent.fsdv1-2.fna.fbcdn.net/v/t1.0-9/c0.242.960.355a/46446863_10156847086899031_7210719975055556608_n.jpg?_nc_cat=105&_nc_ht=scontent.fsdv1-2.fna&oh=404470e8c40099deaf0bbbccbe95525f&oe=5D3F0FA3"></Cover>
+                <Cover user={user}></Cover>
             </div>
         );
     }

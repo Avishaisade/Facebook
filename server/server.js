d0ba7931a1ffdc9340851adbbf37347767aceebb
Avishai <sade.edit@gmail.com>
2019-05-02 14:56:42 +0300

    building mongoDB

--- server/server.js
+++ server/server.js
@@ -1,34 +1,58 @@
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-// db connection
-mongoose.connect("mongodb://localhost:27017/FB-Project", { useNewUrlParser: true });
-
-// routes requirement
-const users = require('./src/users/users.routes');
-const posts = require('./src/Posts/posts.routes');
-
-const express = require('express');
-const cors = require('cors');
-
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const fs = require("fs");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// middlewares
-app.use(bodyParser.json());
-app.use(cors({
-    origin: "http://localhost:3000",
-}));
-app.use(logger('dev'));
-app.use("/users", (req, res, next) => {
-    console.log(`users have been called on: ${new Date()}`);
-    next();
+// db
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", err => {
+    console.log(`DB connection error: ${err.message}`);
 });
 
-// routes
-app.use(users.route);
-app.use(posts.route);
+// routes requirement
+const postRoutes = require("./Routes/posts");
+const authRoutes = require("./Routes/auth");
+const userRoutes = require("./Routes/user");
 
-// serve
+// apiDocs
+app.get("/api", (req, res) => {
+    fs.readFile("docs/apiDocs.json", (err, data) => {
+        if (err) {
+            res.status(400).json({
+                error: err
+            });
+        }
+        const docs = JSON.parse(data);
+        res.json(docs);
+    });
+});
+
+// middleware -
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+app.use("/api", postRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use(function(err, req, res, next) {
+    if (err.name === "UnauthorizedError") {
+        res.status(401).json({ error: "Unauthorized!" });
+    }
+});
+
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});

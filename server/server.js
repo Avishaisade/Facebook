16fa5395c7583ca4ff99acb2e8ce5dd4a425463d
Avishai Sade <sade.edit@gmail.com>
2019-05-25 23:19:09 +0300

    fixed

--- server/server.js
+++ server/server.js
@@ -9,22 +9,18 @@ const fs = require("fs");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-
 // db
 mongoose
     .connect(process.env.MONGO_URI, { useNewUrlParser: true })
     .then(() => console.log("DB Connected"));
-
 mongoose.connection.on("error", err => {
     console.log(`DB connection error: ${err.message}`);
 });
 mongoose.set('useFindAndModify', false);
-
 // routes requirement
 const postRoutes = require("./Routes/posts.route");
 const authRoutes = require("./Routes/auth.route");
 const userRoutes = require("./Routes/user.route");
-
 // apiDocs
 app.get("/api", (req, res) => {
     fs.readFile("docs/apiDocs.json", (err, data) => {
@@ -37,23 +33,22 @@ app.get("/api", (req, res) => {
         res.json(docs);
     });
 });
-
 // middleware -
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 app.use(cors({origin: 'http://localhost:3000',}));
 app.use(postRoutes);
 app.use(authRoutes);
 app.use(userRoutes);
-app.use('/', express.static('build'));
+app.use(express.static('build'));
 app.use(function(err, req, res, next) {
     if (err.name === "UnauthorizedError") {
         res.status(401).json({ error: "Unauthorized!" });
     }
 });
-
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
